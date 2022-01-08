c45d5e6bf516ab1f5ad10ec68ea46ef628b8b1de
Andreas Rozek <A.Rozek@gmx.de>
2022-01-08 17:25:11 +0100

    Update Demo.js

--- Demo.js
+++ Demo.js
@@ -4,10 +4,10 @@
   require('https://raw.githubusercontent.com/rozek/banglejs-2-fillroundrect/main/fillRoundRect.js');
 
   g.setColor('#FF0000');
-  g.fillRoundedRect(20,30, 80,120, 10);
+  g.fillRoundRect(20,30, 80,120, 10);
 
   g.setColor('#0000FF');
-  g.fillRoundedRect(50,140, 130,60, 100);
+  g.fillRoundRect(50,140, 130,60, 100);
 
   g.setColor('#00FF00');
-  g.fillRoundedRect(160,50, 70,80, 20);
+  g.fillRoundRect(160,50, 70,80, 20);
