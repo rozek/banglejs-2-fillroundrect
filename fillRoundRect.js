efb3a56fb4b2d09fc836e2ccf2726aa3fba132c1
Andreas Rozek <A.Rozek@gmx.de>
2022-01-12 06:34:08 +0100

    Update fillRoundRect.js

--- fillRoundRect.js
+++ fillRoundRect.js
@@ -1,6 +1,6 @@
-(function () {
-  if (g.fillRoundRect == null) {
-    g.fillRoundRect = function fillRoundRect (x1,y1, x2,y2, r) {
+;(function () {
+  if (Graphics.prototype.fillRoundRect == null) {
+    Graphics.prototype.fillRoundRect = function fillRoundRect (x1,y1, x2,y2, r) {
       let x,y;
       if (x1 > x2) { x = x1; x1 = x2; x2 = x; }
       if (y1 > y2) { y = y1; y1 = y2; y2 = y; }
